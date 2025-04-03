5acf76a8504ece07a6acc1e52752fd1c0ec8b191
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2025-04-03 16:14:38 +0530

    Updates to teleport effects

--- src/effects/TeleportEffects.ts
+++ src/effects/TeleportEffects.ts
@@ -13,14 +13,16 @@ export class TeleportEffects {
     private createParticles(): void {
         // Create teleport in effect (blue particles converging)
         const teleportIn = this.scene.add.particles(0, 0, 'white-pixel', {
-            speed: { min: 50, max: 150 },
+            speed: { min: 100, max: 200 },
             angle: { min: 0, max: 360 },
-            scale: { start: 0.5, end: 0 },
-            lifespan: 400,
+            scale: { start: 1, end: 0 },
+            lifespan: 600,
             gravityY: 0,
-            quantity: 1,
-            frequency: 15,
-            tint: 0x00ffff,
+            quantity: 5,
+            frequency: 10,
+            tint: [0x00ffff, 0x0099ff], // Mix of cyan and light blue
+            alpha: { start: 1, end: 0 },
+            blendMode: 'ADD',
             emitting: false
         });
 
@@ -28,14 +30,16 @@ export class TeleportEffects {
 
         // Create teleport out effect (blue particles exploding)
         const teleportOut = this.scene.add.particles(0, 0, 'white-pixel', {
-            speed: { min: 100, max: 200 },
+            speed: { min: 200, max: 300 },
             angle: { min: 0, max: 360 },
-            scale: { start: 0, end: 0.5 },
-            lifespan: 400,
+            scale: { start: 0, end: 1 },
+            lifespan: 600,
             gravityY: 0,
-            quantity: 1,
-            frequency: 15,
-            tint: 0x00ffff,
+            quantity: 5,
+            frequency: 10,
+            tint: [0x00ffff, 0x0099ff], // Mix of cyan and light blue
+            alpha: { start: 1, end: 0 },
+            blendMode: 'ADD',
             emitting: false
         });
 
@@ -47,14 +51,14 @@ export class TeleportEffects {
         const teleportOut = this.emitters.get('teleportOut');
         if (teleportOut) {
             teleportOut.setPosition(fromX, fromY);
-            teleportOut.explode(20);
+            teleportOut.explode(30); // Increased particle count
         }
 
         // Play appear effect at end position
         const teleportIn = this.emitters.get('teleportIn');
         if (teleportIn) {
             teleportIn.setPosition(toX, toY);
-            teleportIn.explode(20);
+            teleportIn.explode(30); // Increased particle count
         }
     }
 } 
