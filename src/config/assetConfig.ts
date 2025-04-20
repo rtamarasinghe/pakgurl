1c353c1a62a72d18ad4f1851280495a671b7b582
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2025-04-20 14:56:48 +0800

    Update asset dir

--- src/config/assetConfig.ts
+++ src/config/assetConfig.ts
@@ -1,7 +1,7 @@
 export function getAssetsPath(): string {
     const href = window.location.href;
     if (href.includes('github.io')) {
-        return '/pakgurl/public/assets/';
+        return '/pakgurl/assets/';
     } else if (href.startsWith('http://localhost:8080')) {
         // Development server - assets are served from root
         return '/assets/';
